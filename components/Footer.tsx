ef13dfb9abd4d9ff69cc2e37e25de4bf683e88f2
Ilya Aizenberg <ilyaizen@gmail.com>
2024-09-12 08:57:22 +0300

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import { GithubIcon } from 'lucide-react';
 export function Footer() {
   return (
     <footer className="mt-8 bg-background text-foreground shadow-md">
-      <div className="container mx-auto flex items-center justify-between px-4 py-4">
+      <div className="container mx-auto flex items-center justify-between px-4 py-4 text-xs">
         <div>&copy; 2024 SongScribe. All rights reserved.</div>
         <a
           href="https://github.com/ilyaizen/songscribe-next"
